8b8c41bd168e0b78281f8c1160c3172d4eadc7be
BaptisteCls <baptiste.collesson@gmail.com>
2023-12-11 01:02:39 +0100

    major update v1

--- script/main.js
+++ script/main.js
@@ -4,3 +4,17 @@ function sendMail() {
     var message = $('#contact textarea').val();
     window.location.href = 'mailto:baptiste.collesson.etu@univ-lille.fr?subject=The subject - ' + name + ' (' + email + ')' + '&body=' + message;
 };
+
+function scrollID(element){
+    document.getElementById(element).scrollIntoView({behavior: 'smooth'});
+}
+
+function copyAdress(){
+    navigator.clipboard.writeText("17 rue du Parc Tournebride, 59160 Capinghem, Appartement 22");
+    alert("Copié dans le presse papié : 17 rue du Parc Tournebride, 59160 Capinghem, Appartement 22")
+}
+
+function copyNumber(){
+    navigator.clipboard.writeText("0625320216");
+    alert("Copié dans le presse papié : 0625320216")
+}
